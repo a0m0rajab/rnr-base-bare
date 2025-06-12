2fe113615b3ffa5d2ece03bcf0bd8f66365f9c4f
a0m0rajab <rajab-2@windowslive.com>
2025-06-12 03:32:38 +0300

    chore: update versions and deps

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
+config.resolver.unstable_conditionNames = [ 'browser', 'require', 'react-native', 'web' ]
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = withNativeWind(config, { input: './global.css' })
